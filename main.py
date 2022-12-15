0c2a7b38f92fc3b8ab82b305f63125d575dd5ff3
Lukas Saltenis <lsaltenis@hotmail.com>
2022-12-15 14:00:26 +0100

    .

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
+
 pygame.init()
 width = 500
 height = 500
 pygame.display.set_mode((width,height))
+pygame.display.set_caption("Tic-Tac-Toe")
+
 
 while True:
     for event in pygame.event.get():
-        if event.type == "QUIT":
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
     pygame.display.update()
 
 
