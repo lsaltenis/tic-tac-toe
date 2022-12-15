3618f383546ac57898c2c6e5b2e0219fcdd7f568
Lukas Saltenis <lsaltenis@hotmail.com>
2022-12-15 13:47:56 +0100

    new changes

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello")
+import pygame
+pygame.init()
+width = 500
+height = 500
+pygame.display.set_mode((width,height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == "QUIT":
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 
 
